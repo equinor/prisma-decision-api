19fffd713de2c554bf94d21948a59e9f69c1a8a7
Einar Salomonsen (Bouvet Norge AS) <ESALO@equinor.com>
2026-04-07 15:30:53 +0200

    fix: use azure id when available

--- PrismaApi/PrismaApi.Domain/Dtos/ProjectRoleDtos.cs
+++ PrismaApi/PrismaApi.Domain/Dtos/ProjectRoleDtos.cs
@@ -6,11 +6,39 @@ namespace PrismaApi.Domain.Dtos;
 
 public class ProjectRoleDto
 {
+    private string _userId = string.Empty;
+    private bool _hasAzureId;
+
     [JsonPropertyName("id")]
     public Guid Id { get; set; } = Guid.NewGuid();
     [JsonPropertyName("user_id")]
     [JsonConverter(typeof(UserIdConverter))]
-    public required string UserId { get; set; }
+    public required string UserId
+    {
+        get => _userId;
+        set
+        {
+            if (_hasAzureId)
+            {
+                return;
+            }
+
+            _userId = value;
+        }
+    }
+    [JsonPropertyName("azure_id")]
+    public string? AzureId
+    {
+        get => null;
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                _hasAzureId = true;
+                _userId = value;
+            }
+        }
+    }
     [JsonPropertyName("project_id")]
     public Guid ProjectId { get; set; }
 }
