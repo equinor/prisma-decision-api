575c04885261f073050688d68db2c22fcbee5880
Einar Salomonsen (Bouvet Norge AS) <ESALO@equinor.com>
2026-04-09 13:51:06 +0200

    chore: remove unused claim fields during testing

--- PrismaApi/PrismaApi.Test/Configuration/Constants/TestClaimTypes.cs
+++ PrismaApi/PrismaApi.Test/Configuration/Constants/TestClaimTypes.cs
@@ -2,15 +2,10 @@
 
 public class TestClaimTypes
 {
-    public const string UserPrincipalName =
-        "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn";
-
     public const string AzureUniquePersonId =
         "http://schemas.microsoft.com/identity/claims/objectidentifier";
 
     public const string ApplicationId = "appid";
-
-    public const string Mail = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
 
     public const string Scope = "http://schemas.microsoft.com/identity/claims/scope";
 }
