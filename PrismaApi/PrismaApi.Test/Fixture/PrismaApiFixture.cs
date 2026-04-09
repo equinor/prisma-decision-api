380ba25f99096e04deb645cd560554a409dc6531
EinarSalbouvet <129149934+EinarSalbouvet@users.noreply.github.com>
2026-04-09 13:08:24 +0200

    Update PrismaApi/PrismaApi.Test/Fixture/PrismaApiFixture.cs
    Co-authored-by: rupak katwal <28688139+rupakkatwal@users.noreply.github.com>

--- PrismaApi/PrismaApi.Test/Fixture/PrismaApiFixture.cs
+++ PrismaApi/PrismaApi.Test/Fixture/PrismaApiFixture.cs
@@ -24,7 +24,7 @@ public class PrismaApiFixture : IAsyncLifetime
             Roles = new[] { AppRoles.PrismaDecisionUser }
         };
 
-        SecundaryUser = new TestPersonProfile
+        SecondaryUser = new TestPersonProfile
         {
             Id = Guid.NewGuid().ToString(),
             Name = "Test User A",
