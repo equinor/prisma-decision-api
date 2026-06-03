4b38c896fcb47ad6923ab2a3eb5766b13ea2d129
EinarSalbouvet <einar.salomonsen@bouvet.no>
2026-06-03 13:17:51 +0200

    fix: restore environment variable retrieval in AppDbContextFactory

--- PrismaDotnetApi/PrismaApi.Infrastructure/Context/AppDbContextFactory.cs
+++ PrismaDotnetApi/PrismaApi.Infrastructure/Context/AppDbContextFactory.cs
@@ -9,8 +9,8 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
     public AppDbContext CreateDbContext(string[] args)
     {
-        // var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-        var environment = "Local";
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        
         IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
         var apiPath = Path.Combine(
                 Directory.GetParent(Directory.GetCurrentDirectory())!.FullName,
