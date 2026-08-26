fd79c2c7a0519536f45c7970a393275d8823982f
rupakkatwal <rupak2072@gmail.com>
2026-08-26 08:47:08 +0200

    added option id in policytabledto

--- PrismaDotnetApi/PrismaApi.Domain/Dtos/PolicyTableDtos.cs
+++ PrismaDotnetApi/PrismaApi.Domain/Dtos/PolicyTableDtos.cs
@@ -6,6 +6,8 @@ public class PolicyTableStatesOutgoingDto
 {
     [JsonPropertyName("states")]
     public List<string> States { get; set; } = [];
+    [JsonPropertyName("option_id")]
+    public required string OptionId { get; set; }
 
     [JsonPropertyName("value")]
     public double Value { get; set; }
