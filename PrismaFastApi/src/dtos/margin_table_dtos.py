a664e3dca2def64c03a47ff4d7f27dd90d61150e
EinarSalbouvet <einar.salomonsen@bouvet.no>
2026-09-21 09:40:59 +0200

    feat: enhance get_margins method to handle parent options and improve margin calculations

--- PrismaFastApi/src/dtos/margin_table_dtos.py
+++ PrismaFastApi/src/dtos/margin_table_dtos.py
@@ -5,4 +5,5 @@ from pydantic import BaseModel
 class MarginTableRowDto(BaseModel):
     uncertainty_id: uuid.UUID
     outcome_id: uuid.UUID
+    parent_options: list[uuid.UUID]
     probability: float
